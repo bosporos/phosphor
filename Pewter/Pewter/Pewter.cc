f11d01775ac663e777013e0e6639dd99e31b7d64
bosporos <elektra.eschalier@gmail.com>
2020-02-27 23:21:10 -0800

    Started stormbreak subproj; starting work on Pewter Input so we can get menus up

---
+++ Pewter/Pewter/Pewter.cc
@@ -0,0 +1,11 @@
+//
+// project pewter
+// author Maximilien M. Cura
+//
+
+#include <Pewter/Pewter>
+
+namespace pewter {
+    const char * _raw_version_string = PWT_VERSION;
+    const char * _raw_project_url    = PWT_PROJECT_URL;
+}
